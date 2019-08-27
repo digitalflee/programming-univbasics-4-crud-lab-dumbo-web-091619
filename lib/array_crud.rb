a73420a9c6e64d9a18ba264db39b048aa9786aae
Keenan Alves <keenan.alves@gmail.com>
2019-08-27 16:27:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,27 +3,27 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  rand_array = ['lolipop', 'racket', 'bum', 'beach']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.indexOf
 end
 
 def retrieve_first_element_from_array(array)
