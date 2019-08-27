33941273fb39aab58caddd51b17c14e04710ba88
Keenan Alves <keenan.alves@gmail.com>
2019-08-27 16:31:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  rand_array = ['lolipop', 'racket', 'bum', 'beach']
+  array = ['lolipop', 'racket', 'bum', 'beach']
 end
 
 def add_element_to_end_of_array(array, element)
